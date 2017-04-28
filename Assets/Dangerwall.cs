e048fc223c09dde76631b6a1f10dd83c98a33051
kaede-1008 <s16t203@stu.kagawa-u.ac.jp>
2017-04-28 20:11:54 +0900

    first commit

---
+++ Assets/Dangerwall.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Dangerwall : MonoBehaviour {
+
+	void OnCollisionEnter (Collision hit) {
+
+        if (hit.gameObject.CompareTag("Player")) {
+
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            SceneManager.LoadScene(sceneIndex);
+        }
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 10;
+	void FixedUpdate ()
+	{
+		//    入力をｘとｚに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		//同一のGameObjectが持つRigidbodyコンポーネントを取得
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		//rigidbodyのｘ軸（横）とｚ軸（奥）に力を加える
+		rigidbody.AddForce(x * speed , 0, z * speed);
+	}
+}
